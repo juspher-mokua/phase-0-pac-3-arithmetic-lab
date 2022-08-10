1cd29a600bc8c806847c31ee4c758db64bcf0d19
Juspher Mokua <juspermokua6@gmail.com>
2022-08-10 15:01:10 +0300

    done assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add () {
+    add (a,b)
+}
+function subtract () {
+    subtract (a,b)
+}
+function multiply() {
+    multiply (a,b)
+}
+function divide () {
+    divide (a,b)
+}
+function add(a,b) {
+    return (a+b);
+}
+function subtract(a,b) {
+    return (a-b)
+}
+function multiply(a,b) {
+    return (a*b)
+}
+function divide(a,b) {
+    return (a/b)
+}
+function increment(n) {
+   return (++n)
+}
+function decrement(n) {
+   return (--n)
+}
+function makeInt(n) {
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
